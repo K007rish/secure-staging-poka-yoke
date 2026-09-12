783b0897adf4462980c783f8c49f1aaa6c8a9504
Krish Sutariya <krishsutariya98@gmail.com>
2026-09-12 15:26:18 +0530

    Test

--- ci/test1.py
+++ ci/test1.py
@@ -1,8 +1,5 @@
 # Author: Krish Sutariya
 
 
-#def test_function():
-    #print("This is a clean test.")
-# Author: Krish Sutariya
-
-API_KEY = "FAKE_TEST_API_KEY_1234567890"
+def test_function():
+    print("This is a clean test.")
