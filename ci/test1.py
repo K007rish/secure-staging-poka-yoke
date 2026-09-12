ac0fd1b8bcbdfb1f897d14593efe961655a13a01
Krish Sutariya <krishsutariya98@gmail.com>
2026-09-12 15:22:59 +0530

    Test fail-closed credential gate

--- ci/test1.py
+++ ci/test1.py
@@ -1,5 +1,8 @@
 # Author: Krish Sutariya
 
 
-def test_function():
-    print("This is a clean test.")
+#def test_function():
+    #print("This is a clean test.")
+# Author: Krish Sutariya
+
+API_KEY = "FAKE_TEST_API_KEY_1234567890"
